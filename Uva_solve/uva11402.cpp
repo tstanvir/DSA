bf086765a5616917f532f3407e79f964e0c1b776
Tanvir hussain <24709514+tstanvir@users.noreply.github.com>
2020-05-22 04:42:36 +0600

    new file added

--- Uva_solve/uva11402.cpp
+++ Uva_solve/uva11402.cpp
@@ -2,146 +2,168 @@
 using namespace std;
 #define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
 string s="";
-const int maxx=1024005;
-int segT[4*maxx][2];
-int lazy[4*maxx][2];
+const int maxx=1024005+7;
+int segT[4*maxx];
+int lazy[4*maxx];
 void build(int nodel,int noder,int pos){
-    if(nodel==noder ){
-        segT[pos][s[nodel]-'0']++;
+    if(nodel==noder  ){
+        if(s[nodel]=='1') segT[pos]=1;
         return;
     }
     int mid=nodel+(noder-nodel)/2;
     build(nodel,mid,2*pos+1);
     build(mid+1,noder,2*pos+2);
-    segT[pos][1]=segT[2*pos+1][1]+segT[2*pos+2][1];
-    segT[pos][0]=segT[2*pos+1][0]+segT[2*pos+2][0];
+    segT[pos]=segT[2*pos+1]+segT[2*pos+2];
 
 }
-void update(int ul,int ur,int nodel,int noder ,int pos,char op){
-    if(noder<nodel) return;
-    if(lazy[pos][0]!=0){
-        segT[pos][0]+=lazy[pos][0];
-        if(nodel!=noder)
-        {
-            lazy[2*pos+1][0]+=lazy[pos][0];
-            lazy[2*pos+2][0]+=lazy[pos][0];
+int doFlip(int x) {
+    if( x == 2 ) return 1 ;
+    if( x == 1 ) return 2 ;
+    if( x == 3 ) return 0 ;
+    return 3 ;
+}
+void update1(int ul,int ur,int nodel,int noder,int pos){
+    if(lazy[pos]!=0){
+        if(lazy[pos]==1){
+            segT[pos]=(noder-nodel+1);
         }
-        lazy[pos][0]=0;
-    }
-    if(lazy[pos][1]!=0){
-        segT[pos][1]+=lazy[pos][1];
-        if(nodel!=noder)
-        {
-            lazy[2*pos+1][1]+=lazy[pos][1];
-            lazy[2*pos+2][1]+=lazy[pos][1];
+        if(lazy[pos]==2){
+            segT[pos]=0;
         }
-        lazy[pos][1]=0;
-    }
-    if(op=='F'){
-        if(ul<=nodel && ur>=noder){
-        segT[pos][1]+=segT[pos][0];
-        segT[pos][0]=0;
+        if(lazy[pos]==3){
+            segT[pos]=(noder-nodel+1)-segT[pos];
+        }
         if(nodel!=noder){
-            lazy[2*pos+1][1]+=segT[2*pos+1][0];
-            lazy[2*pos+2][1]+=segT[2*pos+1][0];
-            lazy[2*pos+1][0]+=(-1*segT[2*pos+1][0]);
-            lazy[2*pos+2][0]+=(-1*segT[2*pos+1][0]);
+            if(lazy[pos]==1 || lazy[pos]==2 ) lazy[2*pos+1]=lazy[2*pos+2]=lazy[pos];
+            else if(lazy[pos]==3){
+                lazy[2*pos+1]=doFlip(lazy[2*pos+1]);
+                lazy[2*pos+2]=doFlip(lazy[2*pos+2]);
+            }
+        }
+        lazy[pos]=0;
+    }
+    if(ul>noder || ur<nodel || nodel>noder){
+        return ;
+    }
+    if(ul<=nodel && ur>=noder){
+        segT[pos]=(noder-nodel+1);
+        if(nodel!=noder){
+                lazy[2*pos+1]=1;
+                lazy[2*pos+2]=1;
         }
         return;
     }
-    else if(ul>noder || ur<nodel) return;
-    else{
-        int mid=(nodel+noder)/2;
-        update(ul,ur,nodel,mid,2*pos+1,op);
-        update(ul,ur,mid+1,noder,2*pos+2,op);
-        segT[pos][1]=segT[2*pos+1][1]+segT[2*pos+2][1];
-        segT[pos][0]=segT[2*pos+1][0]+segT[2*pos+2][0];
-
-    }
-    }
-    if(op=='E'){
-        if(ul<=nodel && ur>=noder){
-        segT[pos][0]+=segT[pos][1];
-        segT[pos][1]=0;
+    int mid=(nodel+noder)/2;
+    update1(ul,ur,nodel,mid,2*pos+1);
+    update1(ul,ur,mid+1,noder,2*pos+2);
+    segT[pos]=segT[2*pos+1]+segT[2*pos+2];
+}
+void update2(int ul,int ur,int nodel,int noder,int pos){
+    if(lazy[pos]!=0){
+        if(lazy[pos]==1){
+            segT[pos]=(noder-nodel+1);
+        }
+        if(lazy[pos]==2){
+            segT[pos]=0;
+        }
+        if(lazy[pos]==3){
+            segT[pos]=(noder-nodel+1)-segT[pos];
+        }
         if(nodel!=noder){
-            lazy[2*pos+1][0]+=segT[2*pos+1][1];
-            lazy[2*pos+2][0]+=segT[2*pos+1][1];
-            lazy[2*pos+1][1]+=(-1*segT[2*pos+1][1]);
-            lazy[2*pos+2][1]+=(-1*segT[2*pos+1][1]);
+            if(lazy[pos]==1 || lazy[pos]==2 ) lazy[2*pos+1]=lazy[2*pos+2]=lazy[pos];
+            else if(lazy[pos]==3){
+                lazy[2*pos+1]=doFlip(lazy[2*pos+1]);
+                lazy[2*pos+2]=doFlip(lazy[2*pos+2]);
+            }
+        }
+        lazy[pos]=0;
+    }
+    if(ul>noder || ur<nodel || nodel>noder){
+        return ;
+    }
+    if(ul<=nodel && ur>=noder){
+        segT[pos]=0;
+        if(nodel!=noder){
+                lazy[2*pos+1]=2;
+                lazy[2*pos+2]=2;
         }
         return;
     }
-    else if(ul>noder || ur<nodel) return;
-    else{
-        int mid=(nodel+noder)/2;
-        update(ul,ur,nodel,mid,2*pos+1,op);
-        update(ul,ur,mid+1,noder,2*pos+2,op);
-        segT[pos][1]=segT[2*pos+1][1]+segT[2*pos+2][1];
-        segT[pos][0]=segT[2*pos+1][0]+segT[2*pos+2][0];
-
-    }
-    }
-    if(op=='I'){
-        if(ul<=nodel && ur>=noder){
-        int tmp=segT[pos][0];
-        segT[pos][0]=segT[pos][1];
-        segT[pos][1]=tmp;
+    int mid=(nodel+noder)/2;
+    update2(ul,ur,nodel,mid,2*pos+1);
+    update2(ul,ur,mid+1,noder,2*pos+2);
+    segT[pos]=segT[2*pos+1]+segT[2*pos+2];
+}
+void update3(int ul,int ur,int nodel,int noder,int pos){
+    if(lazy[pos]!=0){
+        if(lazy[pos]==1){
+            segT[pos]=(noder-nodel+1);
+        }
+        if(lazy[pos]==2){
+            segT[pos]=0;
+        }
+        if(lazy[pos]==3){
+            segT[pos]=(noder-nodel+1)-segT[pos];
+        }
         if(nodel!=noder){
-            lazy[2*pos+1][0]+=segT[2*pos+1][1];
-            lazy[2*pos+2][0]+=segT[2*pos+1][1];
-            lazy[2*pos+1][1]+=segT[2*pos+1][0];
-            lazy[2*pos+2][1]+=segT[2*pos+1][0];
+            if(lazy[pos]==1 || lazy[pos]==2 ) lazy[2*pos+1]=lazy[2*pos+2]=lazy[pos];
+            else if(lazy[pos]==3){
+                lazy[2*pos+1]=doFlip(lazy[2*pos+1]);
+                lazy[2*pos+2]=doFlip(lazy[2*pos+2]);
+            }
+        }
+        lazy[pos]=0;
+    }
+    if(ul>noder || ur<nodel || nodel>noder){
+        return ;
+    }
+    if(ul<=nodel && ur>=noder){
+        segT[pos]=(noder-nodel+1)-segT[pos];
+        if(nodel!=noder){
+                lazy[2*pos+1]=doFlip(lazy[2*pos+1]);
+                lazy[2*pos+2]=doFlip(lazy[2*pos+2]);
         }
         return;
     }
-    else if(ul>noder || ur<nodel) return;
-    else{
-        int mid=(nodel+noder)/2;
-        update(ul,ur,nodel,mid,2*pos+1,op);
-        update(ul,ur,mid+1,noder,2*pos+2,op);
-        segT[pos][1]=segT[2*pos+1][1]+segT[2*pos+2][1];
-        segT[pos][0]=segT[2*pos+1][0]+segT[2*pos+2][0];
-
-    }
-    }
-
+    int mid=(nodel+noder)/2;
+    update3(ul,ur,nodel,mid,2*pos+1);
+    update3(ul,ur,mid+1,noder,2*pos+2);
+    segT[pos]=segT[2*pos+1]+segT[2*pos+2];
 }
 int query(int ql,int qr,int nodel,int noder,int pos){
-    if(nodel>noder) return 0;
-    if(lazy[pos][0]!=0){
-        segT[pos][0]+=lazy[pos][0];
-        if(nodel!=noder)
-        {
-            lazy[2*pos+1][0]+=lazy[pos][0];
-            lazy[2*pos+2][0]+=lazy[pos][0];
+    if(lazy[pos]!=0){
+        if(lazy[pos]==1){
+            segT[pos]=(noder-nodel+1);
+            if(nodel!=noder){
+                lazy[2*pos+1]=lazy[pos];
+                lazy[2*pos+2]=lazy[pos];
+            }
         }
-        lazy[pos][0]=0;
+        if(lazy[pos]==2){
+            segT[pos]=0;
+            if(nodel!=noder){
+                lazy[2*pos+1]=lazy[pos];
+                lazy[2*pos+2]=lazy[pos];
+            }
+        }
+        lazy[pos]=0;
     }
-    if(lazy[pos][1]!=0){
-        segT[pos][1]+=lazy[pos][1];
-        if(nodel!=noder)
-        {
-            lazy[2*pos+1][1]+=lazy[pos][1];
-            lazy[2*pos+2][1]+=lazy[pos][1];
-        }
-        lazy[pos][1]=0;
+    if(ql>noder || qr<nodel || nodel>noder){
+        return 0;
     }
     if(ql<=nodel && qr>=noder){
-        return segT[pos][1];
+        return segT[pos];
     }
-    else if(ql>noder || qr<nodel) return 0;
-    else{
-        int mid=(nodel+noder)/2;
-        return query(ql,qr,nodel,mid,2*pos+1)+query(ql,qr,mid+1,noder,2*pos+2);
-    }
+    int mid=(nodel+noder)/2;
+    return query(ql,qr,nodel,mid,2*pos+1)+query(ql,qr,mid+1,noder,2*pos+2);
 }
 int main(){
     int t;
     cin>>t;
     int cs=0;
     while(t--){
-        SET2d(segT,4*maxx,2);
-        SET2d(lazy,4*maxx,2);
+        memset(segT,0,sizeof(segT));
+        memset(lazy,0,sizeof(lazy));
         cout<<"Case "<<++cs<<":"<<endl;
 
         s.clear();
@@ -158,6 +180,9 @@ int main(){
         }
         int sz=s.size();
         build(0,sz-1,0);
+        /*for(int i=0;i<20;i++){
+            cout<<segT[i][0]<<" "<<segT[i][1]<<endl;
+        }*/
         int q;
         cin>>q;
         int qs=0;
@@ -167,17 +192,17 @@ int main(){
             if(op=='F'){
                 int l,r;
                 cin>>l>>r;
-                update(l,r,0,sz-1,0,op);
+                update1(l,r,0,sz-1,0);
             }
             else if(op=='E'){
                     int l,r;
                 cin>>l>>r;
-                update(l,r,0,sz-1,0,op);
+                update2(l,r,0,sz-1,0);
             }
             else if(op=='I'){
                 int l,r;
                 cin>>l>>r;
-                update(l,r,0,sz-1,0,op);
+                update3(l,r,0,sz-1,0);
             }
             else{
                 int l,r;
@@ -185,9 +210,19 @@ int main(){
                 cout<<"Q"<<++qs<<": "<<query(l,r,0,sz-1,0)<<endl;
 
             }
+           /* for(int i=0;i<64;i++){
+            cout<<segT[i][0]<<" "<<segT[i][1]<<endl;*/
         }
+        }
+        /*cout<<endl<<endl;
+        cout<<"segT: "<<endl;
+        for(int i=0;i<2*(pow(2,log2(sz))+1)-1;i++) cout<<segT[i][0]<<" "<<segT[i][1]<<endl;
+        cout<<endl<<"Lazy: "<<endl;
+        for(int i=0;i<2*(pow(2,log2(sz))+1)-1;i++) cout<<lazy[i][0]<<" "<<lazy[i][1]<<endl;
+        cout<<endl<<endl;
+        */
 
-    }
+
 
     return 0;
 }
