d9d0e3907a70845ef0ff8d9d3db394d979aacb01
MD Tanvir hussain <tanvirshossain0@gmail.com>
2021-03-02 19:44:38 +0600

    new file added.

--- DSA_LIbrary.cpp
+++ DSA_LIbrary.cpp
@@ -335,6 +335,63 @@ ll maxQ(int treeL,int treeR,int L,int R,int pos){
 
 
 
+
+//kth smallest number in a range 
+vector<int> seg[4*maxx]; 
+void build(int ci, int st, int end, pair<int, int>* B) 
+{ 
+    if (st == end) { 
+        seg[ci].push_back(B[st].second); 
+        return; 
+    } 
+  
+    int mid = (st + end) / 2; 
+    build(2 * ci + 1, st, mid, B); 
+    build(2 * ci + 2, mid + 1, end, B);  
+    merge(seg[2 * ci + 1].begin(), seg[2 * ci + 1].end(), 
+          seg[2 * ci + 2].begin(), seg[2 * ci + 2].end(), 
+          back_inserter(seg[ci])); 
+} 
+int query(int ci, int st, int end, int l, int r, int k) 
+{ 
+
+    if (st == end) 
+        return seg[ci][0]; 
+    int p = upper_bound(seg[2 * ci + 1].begin(), 
+                        seg[2 * ci + 1].end(), r) 
+            - lower_bound(seg[2 * ci + 1].begin(), 
+                          seg[2 * ci + 1].end(), l); 
+  
+    int mid = (st + end) / 2; 
+    if (p >= k) 
+        return query(2 * ci + 1, st, mid, l, r, k); 
+    else
+        return query(2 * ci + 2, mid + 1, end, l, r, k - p); 
+} 
+void solve(){
+    int n,q;
+    cin>>n>>q;
+    vi arr(n);
+    rep(i,n) cin>>arr[i];
+    pair<int, int> B[n]; 
+  
+    for (int i = 0; i < n; i++) { 
+        B[i] = { arr[i], i }; 
+    } 
+    sort(B, B + n); 
+    build(0, 0, n - 1, B); 
+    while(q--){
+        int l,r,k;
+        cin>>l>>r>>k;
+        l--,r--;
+        cout<<arr[query(0, 0, n - 1, l, r, k)]<<endl;//gives kth smalleset element in the subarray 
+                                                    //starts from l to rth index.
+    }
+}
+
+
+
+
 //bit binary indexed tree
 int bit[maxx];
 void update(int pos,int val,int n){
