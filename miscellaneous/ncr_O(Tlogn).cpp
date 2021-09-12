cfcfaf0c0e1f083739793a28744c596d092a2fa4
MD Tanvir hussain <24709514+tstanvir@users.noreply.github.com>
2021-09-12 19:32:09 +0600

    Update ncr_O(Tlogn).cpp

--- miscellaneous/ncr_O(Tlogn).cpp
+++ miscellaneous/ncr_O(Tlogn).cpp
@@ -1,95 +1,120 @@
-/// Bismillahir Rahmanir Rahim
-//Author: Tanvir Hussain
-//ICE,NSTU
 #include<bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//find_by_order(k): It returns to an iterator to the kth element 
+//(counting from zero) in the set in O(logn) time.
+//To find the first element k must be zero.
+
+// order_of_key(k) : It returns to the number of items that 
+// are strictly smaller than our item k in O(logn) time.
+//using namespace __gnu_pbds;
 using namespace std;
-const long long MOD = 1000003;
+
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define IOS ios::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
 #define SET(x) memset(x, 0, sizeof(x))
-#define SET2d(x,m,n) memset(x, 0, sizeof(x[0][0]) * m * n)
-#define SETBOOL(x) memset(x,false,sizeof(x))
 #define CLR(x) memset(x, -1, sizeof(x))
-#define CLR2d(x,m,n) memset(x, -1, sizeof(x[0][0]) * m * n)
 #define mp make_pair
-#define PII pair<int, int>
-#define pf printf
-#define sf scanf
 #define ALL(x) x.begin(),x.end()
 #define pb push_back
-#define IOS ios::sync_with_stdio(false); cin.tie(0);
-#define np std::string::npos
+#define ppb pop_back
 #define highest(x) numeric_limits<x>::max()
 #define lowest(x) numeric_limits<x>::min()
-#define Inf INFINITY
 #define minv(v) *min_element(v.begin(),v.end())
 #define maxv(v) *max_element(v.begin(),v.end())
-#define cases(cs,t) for(int cs=1;cs<=t;cs++)
 #define PI acos(-1)
-#define no1 __builtin_popcount
-#define BOUNDARY(i, j) ((i >= 0 && i < row) && (j >= 0 && j < column))
+#define border(i, j , row, column) ((i >= 0 && i < row) && (j >= 0 && j < column))
 #define uniq(vec) vec.resize(distance(vec.begin(),unique(vec.begin(),vec.end())))
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 #define sz(a) int(a.size())
 #define ff first
 #define ss second
 #define endl "\n"
 #define forch(it,s) for(auto it:s)
-#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
 #define rep(i,a) for(int i=0; i<a;i++)
 #define rep1(i,a,b) for(int i=(a);i<=(b);++i)
 #define irep(i,b,a) for(int i=(b);i>=(a);--i)
-#define bits(n) __builtin_popcount(n)
+#define no1(n) __builtin_popcount(n)
 #define maxpq priority_queue<int>
 #define minpq priority_queue<int, vector<int>, greater<int> >
+#define di deque<int>
+#define dll deque<ll>
+#define pf push_front
+#define ppf pop_front
+#define preci cout<<fixed<<setprecision(9);
+ 
+ 
+vector<string> vec_splitter(string s) {
+    s += ',';
+    vector<string> res;
+    while(!s.empty()) {
+        res.push_back(s.substr(0, s.find(',')));
+        s = s.substr(s.find(',') + 1);
+    }
+    return res;
+}
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx,
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+    if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+    stringstream ss; ss << H;
+    cerr << args[idx] << " = " << ss.str();
+    debug_out(args, idx + 1, LINE_NUM, T...);
+}
 
+#define XOX
+#ifdef XOX
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+ 
+ 
 
-int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
 typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<pair<int,int>> vpii;
-typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+typedef vector<pair<ll,ll>> vpll;
+//typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> omst;
+//typedef tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ost; 
 int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
 int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
 int dx4[] = {0, 0, 1, -1};
 int dy4[] = {1, -1, 0, 0};
 const int maxx=1000005;
+const long long MOD = 1000003;
+const double rad=(acos(-1)/180.00);
+const int INF    = 0x3f3f3f3f;
+const ll LL_INF  = 0x3f3f3f3f3f3f3f3f;
+#define EPS  0.000000001
 
-//this fuction sorts vector pair according to first element in descending order.
+ 
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+ll gcd(ll a, ll b) { if (a == 0) return b; return gcd(b % a, a);}
+ll lcm(ll a, ll b) {return (a*b)/gcd(a,b);}
+double sq(double x) {return x*x;} 
+ll po(ll b,ll p){ ll res=1; while(p){ res*=b; p--;} return res;}
+ll lg2(ll x){ ll res=0; while(x>1){ res++; x/=2ll;} return res;}
+bool get_bit(int mask,int pos) {return mask&(1<<pos);}
 bool sortinrev(const pair<int,int> &a,const pair<int,int> &b)
 {
     return a.first>b.first;
 }
-
-template<typename T>inline T Bigmod(T base, T power, T MOD){
-    T ret=1;
-    while(power)
-    {
-        if(power & 1)ret=(ret*base)%MOD;
-        base=(base*base)%MOD;
-        power>>=1;
-    }
-    return ret;
-}
-double sq(double x) {return x*x;}
-ll po(ll b,ll p){
-    ll res=1;
-    while(p){
-        res*=b;
-        p--;
-    }
-    return res;
-}
-ll lg2(ll x){
-    ll res=0;
-    while(x>1){
-        res++;
-        x/=2ll;
-    }
-    return res;
-}
+int cs=0;
 vll fact(maxx);
 void init(){
     fact[0]=1;
@@ -102,37 +127,29 @@ ll ncr(ll n,ll k){
     if(k==0 || n-k==0) return 1;
     ll res=fact[n];
     //cout<<n<<" "<<fact[n]<<endl;
-    ll denom= (Bigmod(fact[k],MOD-2,MOD)+MOD)%MOD*(Bigmod(fact[n-k],MOD-2,MOD)+MOD)%MOD;
-    denom%=MOD;
+   
+    ll denom=modMul(modPow(fact[k],MOD-2),modPow(fact[n-k],MOD-2));
+    normal(denom);
     return (res%MOD*denom%MOD)%MOD;
 }
 void solve(){
-
+    cout<<"Case "<<++cs<<": ";
     ll n,k;
-    sf("%lld%lld",&n,&k);
-    pf("%lld\n",ncr(n,k));
+    cin>>n>>k;
+    cout<<ncr(n,k)<<endl;
 }
-
+ 
 signed main()
-
+ 
 {
     IOS;
-    init();
-    /*#ifndef ONLINE_JUDGE
-        freopen ("data.in","r",stdin);
-        freopen ("data.out","w",stdout);
-    #endif*/
     int t;
-    sf("%d",&t);
-    int cs=0;
+    t=1;
+    init();
+    cin>>t;
     while(t--){
-        pf("Case %d: ",++cs);
         solve();
     }
-
-     return 0;
-
+    return 0;
+ 
 }
-///Alhamdulillah
-
-
